67403cc290056595f069a290997f8f3aa9635bfc
Github <janpeter@tammingas.nl>
2015-04-21 21:54:48 +0200

    main module name edited

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -6,36 +6,36 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /reader when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/reader");
   });
 
 
-  describe('view1', function() {
+  describe('reader', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/reader');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render reader when user navigates to /reader', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/reader/);
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('library', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view2');
+      browser.get('index.html#/library');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render library when user navigates to /library', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+        toMatch(/library/);
     });
 
   });
